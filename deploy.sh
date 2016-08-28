9b08b4b108220a3f6b7fe9d283ca29c4f5e2aee7
Eduard Neculaesi <neculaesi.eduard@gmail.com>
2016-08-28 19:36:45 +0300

    Add deploy script for gh-pages

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+npm run build
+
+git checkout -B gh-pages
+git add -f build
+git commit -am "Rebuild website"
+git filter-branch -f --prune-empty --subdirectory-filter build
+git push -f origin gh-pages
+git checkout -
+git reset
+
+rm -rf build
